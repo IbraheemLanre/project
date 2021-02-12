9e72db0b0bc049c15e085b8a05f8e31785b1c55d
IbraheemLanre <s2001108@edu.bc.fi>
2021-02-13 00:36:29 +0200

    added redux reducers

---
+++ react-redux/redux-learning/src/redux/reducers/todosReducer.js
@@ -0,0 +1,43 @@
+import { ADD_TODO, TOGGLE_TODO } from "../actionTypes";
+
+const initialState = {
+  allIds: [],
+  byIds: {},
+};
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO: {
+      // const id = action.payload.id
+      // const content = action.payload.content
+      /*Using destructuring */
+      const { id, content } = action.payload;
+      return {
+        ...state,
+        allIds: [...state.allIds, id],
+        byIds: {
+          ...state.byIds,
+          [id]: { content, completed: false },
+        },
+      };
+    }
+
+    case TOGGLE_TODO: {
+      const { id } = action.payload;
+      return {
+        ...state,
+        byIds: {
+          ...state.byIds[id],
+          [id]: {
+            ...state.byIds[id],
+            completed: !state.byIds[id].completed,
+          },
+        },
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;

8b87bc9ea1e496182f23b19983fb833b9bb7b119
IbraheemLanre <s2001108@edu.bc.fi>
2021-02-16 15:06:05 +0200

    created initial setup and mariadb database

---
+++ node/bookdatabaseproject/bookcreatestatement.sql
@@ -0,0 +1,19 @@
+drop database if exists bookdb;
+create database bookdb;
+use bookdb;
+
+create table book(
+    bookId integer not null primary key,
+    name varchar(62) not null,
+    author varchar(12) not null,
+    type varchar(26) not null,
+    year integer not null
+);
+
+drop user if exists 'adrain'@'localhost';
+create user 'adrian'@'localhost' identified by 'tN5LKSaK';
+
+grant all privileges on bookdb.* to 'adrian'@'localhost';
+
+insert into book values(1,'The beautiful ones are not yet born', 'Ayi Kwei Armah', 'Novel', 1968);
+insert into book values(2, 'The gods are not to blame', 'Ola Rotimi', 'Novel', 1971);

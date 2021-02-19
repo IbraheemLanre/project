313d384e67331d8add082802d0247715f35ed59f
IbraheemLanre <s2001108@edu.bc.fi>
2021-02-19 22:06:16 +0200

    added styling

--- node/bookdatabaseproject/index.js
+++ node/bookdatabaseproject/index.js
@@ -13,6 +13,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "webpages"));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("menu");

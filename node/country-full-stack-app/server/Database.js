6cdf28a159a3d6061b9e45babf579d0e9139664b
IbraheemLanre <s2001108@edu.bc.fi>
2021-02-22 01:43:04 +0200

    added database connection

---
+++ node/country-full-stack-app/server/Database.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const mariadb = require("mariadb");
+
+class Database {
+  options = "";
+  connection = "";
+  constructor(options) {
+    this.options = options;
+  }
+
+  dbQueryConnection(sql, parameters, connection) {
+    return new Promise(async (resolve, reject) => {
+      let isConnected = false;
+      try {
+        if (!connection) {
+          conncection = await mariadb.createConnection(this.options);
+          isConnected = true;
+        }
+
+        let queryResult = await connection.query(sql, parameters);
+        if (typeof queryResult === "undefined") {
+          reject("QueryError");
+        } else if (typeof queryResult.affectedRows === "undefined") {
+          delete queryResult.meta;
+          resolve({ queryResult, resultSet: true });
+        } else {
+          resolve({
+            queryResult: {
+              rowsChanged: queryResult.affectedRows,
+              insertId: queryResult.insertId,
+              status: queryResult.warningStatus,
+            },
+            resultSet: false,
+          });
+        }
+      } catch (err) {
+        reject(`Sql-error:${err}`);
+      } finally {
+        if (conncection && isConnected) {
+          connection.end();
+        }
+      }
+    });
+  }
+}

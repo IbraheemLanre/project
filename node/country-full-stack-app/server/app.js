6d829bb703bb836d6b583fd515d53b2038abef8c
IbraheemLanre <s2001108@edu.bc.fi>
2021-02-22 03:22:48 +0200

    created index.js and rendered country data to client-side

--- node/country-full-stack-app/server/app.js
+++ node/country-full-stack-app/server/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const http = require("http");
+const cors = require('cors')
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -8,13 +9,14 @@ const app = express();
 const storage = require("./datastoragelayer");
 
 const server = http.createServer(app);
+
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-2;
 
 app.get("/getall", (req, res) => {
   storage
     .getAll()
-    .then((result) => res.json(result))
+    .then((result) => res.json({data:result}))
     .catch((err) => res.json(err));
 });
 

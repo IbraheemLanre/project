9b8b6c12a5032813a46de12ae5e07b65804da64e
IbraheemLanre <s2001108@edu.bc.fi>
2021-03-05 04:00:07 +0200

    created the html page for drawing chart

---
+++ node/vantaajob/app.js
@@ -0,0 +1,34 @@
+/**
+ * Contains logic for backend data handling and api consumption
+ */
+"use strict";
+
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+const fetch = require("node-fetch");
+dotenv.config();
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+const server = http.createServer(app);
+
+app.use(cors());
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "vantaajob.html"))
+);
+
+//route
+app.get("/json", (req, res) =>
+  fetch("http://gis.vantaa.fi/rest/tyopaikat/v1")
+    .then((result) => result.json())
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err))
+);
+server.listen(port, host, () =>
+  console.log(`Server localhost is ready and listening to port:${port}`)
+);
--- node/vantaajob/index.js
+++ node/vantaajob/index.js
@@ -1,31 +1,20 @@
+/**
+ * Contains logic for frontend data handling
+ */
+
 "use strict";
 
-const http = require("http");
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const dotenv = require("dotenv");
-const fetch = require("node-fetch");
-dotenv.config();
-const port = process.env.PORT;
-const host = process.env.HOST;
+const { response } = require("express");
 
-const server = http.createServer(app);
+document.addEventListener("DOMContentLoaded", init);
 
-app.use(cors());
-
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "vantaajob.html"))
-);
-
-//route
-app.get("/json", (req, res) =>
-  fetch("http://gis.vantaa.fi/rest/tyopaikat/v1")
+function init() {
+  fetch("http://localhost:8080/json", { mode: "cors" })
     .then((result) => result.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.log(err))
-);
-server.listen(port, host, () =>
-  console.log(`Server localhost is ready and listening to port:${port}`)
-);
+    .then((data) => drawAllBars(data))
+    .catch((err) => console.log(err));
+}
+
+function drawAllBars(data){
+    
+}

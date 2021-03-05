f837db06e99d8eb1c9afb4eadba8964a3f9ae359
IbraheemLanre <s2001108@edu.bc.fi>
2021-03-05 02:51:04 +0200

    removed gitignore files

---
+++ node/vantaajob/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+const server = http.createServer(app);
+
+app.use(cors());
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "vantaajob.html"))
+);
+
+
+server.listen(port, host, () =>
+  console.log(`Server localhost is ready and listening to port:${port}`)
+);

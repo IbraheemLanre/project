8324d9d8bf197fdec86403874ea6f5e7eb0314b2
IbraheemLanre <s2001108@edu.bc.fi>
2021-03-06 00:25:01 +0200

    added final code

--- node/vantaajobdisk/app.js
+++ node/vantaajobdisk/app.js
@@ -3,18 +3,42 @@
 const http = require("http");
 const path = require("path");
 const cors = require("cors");
+const fetch = require("node-fetch");
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const { writeToStorage, readFromStorage } = require("./jsonreaderwriterx");
 dotenv.config();
 const host = process.env.HOST;
 const port = process.env.PORT;
+const dataPath = "vantaadata.json";
 
 const server = http.createServer(app);
 app.use(cors());
 
+function checkUpdate() {
+  return true;
+}
+
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "vantaajob.html"))
 );
+
+app.get("/json", async (req, res) => {
+  if (checkUpdate()) {
+    try {
+      const result = await fetch("http://gis.vantaa.fi/rest/tyopaikat/v1");
+      const data = await result.json();
+      await writeToStorage(dataPath, data);
+      res.json(data);
+      console.log("Updated");
+    } catch (err) {
+      console.log("update failed", err.message);
+      res.sendStatus = 404;
+    }
+  }
+  const storage = await readFromStorage(dataPath);
+  res.json(storage);
+});
 
 server.listen(port, host, () => console.log(`Server is up and running`));
---
+++ node/vantaajobdisk/index.js
@@ -0,0 +1,41 @@
+/**
+ * Contains logic for frontend data handling
+ */
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  fetch("/json", { mode: "cors" })
+    .then((result) => result.json())
+    .then((data) => drawAllBars(data))
+    .catch((err) => console.log(err));
+}
+
+// draw chart
+function drawAllBars(data) {
+  if (data.length === 0) {
+    document.getElementById("title").textContent = "No jobs available";
+  }
+  let drwaingCanvas = document.querySelector("#drawingcanvas");
+  let ctx = drwaingCanvas.getContext("2d");
+
+  ctx.translate(10, 150);
+  ctx.fillStyle = "blue";
+  ctx.scale(1.2, 1.2);
+  for (let i = 1, x = 10; i < data.length; i++, x += 30) {
+    drawBar(ctx, x, data[i]);
+  }
+}
+
+// draw a bar
+function drawBar(ctx, x, data) {
+  ctx.fillRect(x, -data.lukumäärä, 20, data.lukumäärä);
+  ctx.save();
+  ctx.fillStyle = "black";
+  ctx.fillText(data.lukumäärä, x + 5, -data.lukumäärä - 10);
+  ctx.rotate(Math.PI / 2);
+  ctx.fillText(data.ammattiala, 0, -x - 5);
+  ctx.restore();
+}

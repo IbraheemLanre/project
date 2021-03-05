ecda5aa2916449565a99f1de5964c1d77e8b272a
IbraheemLanre <s2001108@edu.bc.fi>
2021-03-05 04:17:08 +0200

    added functios for bars and chart and final code

--- node/vantaajob/index.js
+++ node/vantaajob/index.js
@@ -4,7 +4,6 @@
 
 "use strict";
 
-const { response } = require("express");
 
 document.addEventListener("DOMContentLoaded", init);
 
@@ -15,6 +14,26 @@ function init() {
     .catch((err) => console.log(err));
 }
 
-function drawAllBars(data){
-    
+// draw chart
+function drawAllBars(data) {
+  let drwaingCanvas = document.querySelector("#drawingcanvas");
+  let ctx = drwaingCanvas.getContext("2d");
+
+  ctx.translate(10, 150);
+  ctx.fillStyle = "blue";
+  ctx.scale(1.2, 1.2);
+  for (let i = 1, x = 10; i < data.length; i++, x += 30) {
+    drawBar(ctx, x, data[i]);
+  }
+}
+
+// draw a bar
+function drawBar(ctx, x, data) {
+  ctx.fillRect(x, -data.lukumäärä, 20, data.lukumäärä);
+  ctx.save();
+  ctx.fillStyle = "black";
+  ctx.fillText(data.lukumäärä, x + 5, -data.lukumäärä - 10);
+  ctx.rotate(Math.PI / 2);
+  ctx.fillText(data.ammattiala, 0, -x - 5);
+  ctx.restore();
 }

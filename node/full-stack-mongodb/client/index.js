b296e555bfe34c2e82ff617aa4307dc334ab6212
IbraheemLanre <s2001108@edu.bc.fi>
2021-03-08 12:24:30 +0200

    added delete and update logic

--- node/full-stack-mongodb/client/index.js
+++ node/full-stack-mongodb/client/index.js
@@ -80,6 +80,53 @@ app.post("/remove", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+app.get("/updatepost", (req, res) => {
+  res.render("form", {
+    title: "Update Bio Data",
+    header: "Bio Data",
+    action: "/updatedata",
+    name: { value: "", readonly: "" },
+    age: { value: "", readonly: "readonly" },
+  });
+});
+
+app.post("/updatedata", (req, res) => {
+  const name = req.body.name;
+  const url = `http://localhost:8080/update${name}`;
+
+  const result = fetch(url, { mode: "cors" });
+  result
+    .then((res) => res.json())
+    .then(
+      res.render("form", {
+        title: "Update Bio Data",
+        header: "Bio Data",
+        action: "/update",
+        name: { value: "", readonly: "readonly" },
+        age: { value: "", readonly: "" },
+      })
+    )
+    .catch((err) => console.log(err));
+});
+
+app.post("/update", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const url = `http://localhost:8080/update/${name}`;
+    const options = {
+      headers: { "Content-type": "application/json" },
+      method: "PUT",
+      mode: "cors",
+      body: JSON.stringify(req.body),
+    };
+    const result = await fetch(url, options);
+    const data = await result.json();
+    res.render("statuspage", { status: data });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 server.listen(port, host, () =>
   console.log(`Server ${host} is listening to port:${port}`)
 );

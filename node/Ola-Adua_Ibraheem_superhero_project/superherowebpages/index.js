2f73951b93b9fe65909d9f0a3d95c6279d4fb470
IbraheemLanre <s2001108@edu.bc.fi>
2021-03-09 03:18:21 +0200

    added allpages.ejs and get all api

--- node/Ola-Adua_Ibraheem_superhero_project/superherowebpages/index.js
+++ node/Ola-Adua_Ibraheem_superhero_project/superherowebpages/index.js
@@ -6,6 +6,7 @@ const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
 const { port, host } = require("./config.json");
+const API_URL = "http://localhost:4000/superheroes";
 
 const server = http.createServer(app);
 
@@ -17,4 +18,21 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.render("menu"));
 
+app.get("/all", async (req, res) => {
+  try {
+    const result = await fetch(API_URL, { mode: "cors" });
+    const data = await result.json();
+    res.render("allpages", { data });
+  } catch (err) {
+    fetchError(res);
+  }
+});
+
+// app.route()
+
 server.listen(port, host, () => console.log(`Server ${host}:${port} is ready`));
+
+function fetchError(res) {
+  const status = { message: "failed to fetch", type: "error" };
+  res.render("statuspage", { status });
+}

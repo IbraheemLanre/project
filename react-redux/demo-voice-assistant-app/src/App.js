4a9c787ed1eff379abb92ab8010d0964bca66073
IbraheemLanre <s2001108@edu.bc.fi>
2021-03-24 03:16:10 +0200

    added components and styles

--- react-redux/demo-voice-assistant-app/src/App.js
+++ react-redux/demo-voice-assistant-app/src/App.js
@@ -1,23 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
 
-const alanKey = "3c78f9ef36081fa6389430209a09e56a2e956eca572e1d8b807a3e2338fdd0dc/stage";
-const newsKey = 'b1f5497f9cf44b24922304484871e59a'
+const alanKey =
+  "3c78f9ef36081fa6389430209a09e56a2e956eca572e1d8b807a3e2338fdd0dc/stage";
+// const newsKey = "b1f5497f9cf44b24922304484871e59a";
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand:({command})=>{
-          if(command==='testCommand'){
-              alert('This code was executed')
-          }
-      }
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+        }
+      },
     });
   }, []);
+
   return (
     <div>
       <h1>Alan AI News App</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 };
---
+++ react-redux/demo-voice-assistant-app/src/components/NewsCard/NewsCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import newsImage from "../images/news.png";
+import useStyles from "./styles";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
+  const classes = useStyles();
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={urlToImage || newsImage} />
+        <div>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+        <Typography gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+        <Typography variant="h5" color="textSecondary">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default NewsCard;

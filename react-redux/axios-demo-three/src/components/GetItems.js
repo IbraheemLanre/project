f3e6427861f5b19e303ae1385909ada20c27060f
IbraheemLanre <s2001108@edu.bc.fi>
2021-03-28 02:38:03 +0200

    added loading text

--- react-redux/axios-demo-three/src/components/GetItems.js
+++ react-redux/axios-demo-three/src/components/GetItems.js
@@ -7,54 +7,56 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { StyledTableCell, StyledTableRow, useStyles } from "./ItemStyles.js";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const GetItems = () => {
   const [lists, setLists] = useState([]);
-  const showAlert = false;
   const mounted = useRef(true);
+  const [loading, setLoading] = useState(true);
 
   const classes = useStyles();
 
   useEffect(() => {
-    mounted.current = true;
-    if (lists.length && !showAlert) {
-      return;
-    }
     getList().then((groceryItems) => {
       if (mounted.current) {
         setLists(groceryItems);
       }
     });
-    return () => (mounted.current = false);
-  }, [showAlert, lists]);
+
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
 
   return (
     <div className={classes.itemTable}>
       <h1 className={classes.title}>Grocery Items</h1>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>ItemId</StyledTableCell>
-              <StyledTableCell>Item</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {lists.map((list) => (
-              <StyledTableRow key={list.id}>
-                <StyledTableCell>{list.id}</StyledTableCell>
-                <StyledTableCell>{list.item}</StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>ItemId</StyledTableCell>
+                <StyledTableCell>Item</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {lists.map((list) => (
+                <StyledTableRow key={list.id}>
+                  <StyledTableCell>{list.id}</StyledTableCell>
+                  <StyledTableCell>{list.item}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+
       <Link to="/">
         <input className={classes.inputSubmit} type="button" value="Menu" />
       </Link>
     </div>
-
   );
 };
 

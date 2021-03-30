ed7e6e2ede96616c8cd055b1bc4f8838922887a4
IbraheemLanre <s2001108@edu.bc.fi>
2021-03-30 05:23:30 +0300

    added state management using hook

---
+++ react-redux/state-mgt/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Product from "./components/Products";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hook State Management</h1>
+      <h2>Shopping Cart Example</h2>
+      <Product/>
+    </div>
+  );
+};
+
+export default App;

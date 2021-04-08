e0247d783db404b8018c5414252ddc123ab1e1b9
IbraheemLanre <s2001108@edu.bc.fi>
2021-04-08 12:11:07 +0300

    update user.controller component

--- node/backendteam5/controllers/user.controller.js
+++ node/backendteam5/controllers/user.controller.js
@@ -2,10 +2,6 @@ exports.AllUsers = (req, res) => {
   res.status(200).send("Public Content.");
 };
 
-exports.userProfile = (req, res)=>{
-    res.status(200).send("User Profile.")
-}
-
 exports.employeeProfile = (req, res) => {
   res.status(200).send("Employee Profile.");
 };

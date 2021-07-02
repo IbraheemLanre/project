3c54d077e6f161f80b69218d2d803b6f7a66b5bb
IbraheemLanre <s2001108@edu.bc.fi>
2021-07-02 16:06:31 +0300

    react login authentication using sessionStorage

--- react-redux/auth-tutorial/src/components/App/App.js
+++ react-redux/auth-tutorial/src/components/App/App.js
@@ -3,11 +3,19 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login'
+import useToken from './useToken';
 import './App.css'
 
 
-
 const App = () => {
+
+    const { token, setToken} = useToken()
+
+    if(!token) {
+        return <Login setToken={setToken}/>
+    }
+
     return (
         <div className="wrapper">
             <h1>Application</h1>
